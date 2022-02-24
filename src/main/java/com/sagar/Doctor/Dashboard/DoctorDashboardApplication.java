a706f381eff406d7711ab7d03e86c3861637b78b
sagarsingh24 <sagar.singh@nineleaps.com>
2022-02-24 15:58:49 +0530

    patient table

---
+++ src/main/java/com/sagar/Doctor/Dashboard/DoctorDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.sagar.Doctor.Dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagar/Doctor/Dashboard/DoctorDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.Doctor.Dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
