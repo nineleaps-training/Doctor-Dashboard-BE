7b55c19ce7623d4da318bea40b7af2f63ddfeef8
pranay-nareddy <pranay.nareddy@nineleaps.com>
2022-02-24 16:56:34 +0530

    doctor login table created

---
+++ src/main/java/com/Spring/SecurityLogin/Entity/DoctorLoginDetails.java
@@ -0,0 +1,27 @@
+package com.Spring.SecurityLogin.Entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "doctor_login_details",uniqueConstraints = @UniqueConstraint(columnNames = {"emailId"}))
+public class DoctorLoginDetails {
+    @Id
+    private long id;
+    @Column(name="firstName",nullable = false)
+    private String firstName;
+    private String lastName;
+    @Column(name="emailId",nullable = false)
+    private String emailId;
+
+    private String domain;
+    private String profileId;
+
+}
